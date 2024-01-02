03994488dcdfe3f4d9bff4499f7bb9008295d19a
Neesty <mysteriousneutrino@gmail.com>
2024-01-02 14:27:30 +0500

    smape 1 ver2 | add errstate

--- intern/smape/smape_1.py
+++ intern/smape/smape_1.py
@@ -1,8 +1,7 @@
 import numpy as np
 
 
-def smape(y_true: np.array, y_pred: np.array) -> float:
-    with np.errstate(divide='ignore'):
-        return np.mean(np.where(y_true + y_pred != 0, 2 * np.abs(y_true - y_pred) / (np.abs(y_true) + np.abs(y_pred)), 0))
-
-
+def smape(y_true: np.array, y_pred: np.array):
+    with np.errstate(divide='ignore', invalid='ignore'):
+        return np.mean(
+            np.where(y_true + y_pred != 0, 2 * np.abs(y_true - y_pred) / (np.abs(y_true) + np.abs(y_pred)), 0))
