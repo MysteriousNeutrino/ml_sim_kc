3d884bc64d1bc97d0b8b91edeaeb16e7ffc84f4a
Neesty <mysteriousneutrino@gmail.com>
2024-01-02 14:21:18 +0500

    smape 1 ver1 | add where

---
+++ intern/smape/smape_1.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def smape(y_true: np.array, y_pred: np.array):
+    return np.mean(np.where(y_true + y_pred != 0, 2 * np.abs(y_true - y_pred) / (np.abs(y_true) + np.abs(y_pred)), 0))
