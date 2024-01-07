f8639bf9843d2fd0aa7aa2a519a2ef45ca263c61
Neesty <mysteriousneutrino@gmail.com>
2024-01-07 20:39:16 +0500

    mutable word count 2 ver1 | done

--- intern/mutable word count/test_utils.py
+++ intern/mutable word count/test_utils.py
@@ -16,6 +16,9 @@ def test_word_count_tricky():
     Check that the function is not mutating the default argument
     :return:
     """
+    batch = ["hello world", "hello world", "hello world"]
+    assert utils.word_count(batch) == {"hello": 3, "world": 3}
+    assert utils.word_count(batch) == {"hello": 3, "world": 3}
     assert utils.word_count("hello world!") == \
            {'h': 1, 'e': 1, 'l': 3, 'o': 2, 'w': 1, 'r': 1, 'd': 1, '!': 1}
     assert utils.word_count("hello world!") == \
--- intern/mutable word count/utils.py
+++ intern/mutable word count/utils.py
@@ -1,23 +1,13 @@
-def word_count(texts):
-    count = {}
-    for text in texts:
-        for word in text.split():
-            count[word] = count.get(word, 0) + 1
-    return count
+def word_count(batch, count=None):
+    """
+    a function that count the number of words in a text
+    :param batch: text where you need to count the number of words
+    :param count: batch size
+    :return: frequency dictionary
+    """
+    count = count or {}  # alernative to: if count is None: count = {}
 
-
-print(word_count("hello world!"))
-print(word_count("hello world!"))
-
-
-def word_count_2(batch, count=None):
-    if count is None:
-        count = {}
     for text in batch:
         for word in text.split():
             count[word] = count.get(word, 0) + 1
     return count
-
-
-print(word_count_2("hello world!"))
-print(word_count_2("hello world!"))
