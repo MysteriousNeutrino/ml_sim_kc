6251d4fa8ba119d6234adf580bc80353f132f842
Neesty <mysteriousneutrino@gmail.com>
2024-01-08 19:43:03 +0500

    smape 1 ver3 | done

--- intern/smape/smape_1.py
+++ intern/smape/smape_1.py
@@ -2,6 +2,16 @@ import numpy as np
 
 
 def smape(y_true: np.array, y_pred: np.array):
-    with np.errstate(divide='ignore', invalid='ignore'):
-        return np.mean(
-            np.where(y_true + y_pred != 0, 2 * np.abs(y_true - y_pred) / (np.abs(y_true) + np.abs(y_pred)), 0))
+    """
+
+    :param y_true:
+    :param y_pred:
+    :return:
+    """
+    numerator = 2 * np.abs(y_true - y_pred)
+    denominator = np.abs(y_true) + np.abs(y_pred)
+
+    # Заменяем деление на ноль на ноль
+    result = np.where(denominator != 0, numerator / denominator, 0)
+
+    return np.mean(result)
