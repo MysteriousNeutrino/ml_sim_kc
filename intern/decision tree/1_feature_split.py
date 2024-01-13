393943f0a5e4259154eee95fdf7f810b38dc1c96
Neesty <mysteriousneutrino@gmail.com>
2024-01-13 21:26:10 +0500

    decision tree 3 ver2 | reference solution

--- intern/decision tree/1_feature_split.py
+++ intern/decision tree/1_feature_split.py
@@ -1,37 +1,41 @@
+from __future__ import annotations
+
 import numpy as np
 
 
-def split(X: np.ndarray, y: np.ndarray, feature: int) -> float:
-    """Find the best split for a node (one feature)"""
-
-    split_elements = X[:, feature]
-
-    best_split = {}
-    for i in split_elements:
-        indexes_below_split_value = np.where(split_elements <= i)[0]
-        indexes_above_split_value = np.where(split_elements > i)[0]
-
-        y_left = np.array(y[indexes_below_split_value])
-        y_right = np.array(y[indexes_above_split_value])
-
-        mse = weighted_mse(y_left, y_right)
-        best_split[i] = mse
-
-    max_key = min(best_split, key=best_split.get)
-
-    return max_key
-
-
-def mse(y: np.ndarray) -> np.ndarray:
-    """Compute the mean squared error of a vector."""
-
-    y_pred = np.mean(y)
-
-    return np.mean((y - y_pred) ** 2)
+def mse(y: np.ndarray) -> float:
+    """Compute the mse impurity criterion for a given set of target values."""
+    return np.mean((y - np.mean(y)) ** 2)
 
 
 def weighted_mse(y_left: np.ndarray, y_right: np.ndarray) -> float:
-    """Compute the weighted mean squared error of two vectors."""
-    y_left_size = y_left.size
-    y_right_size = y_right.size
-    return (mse(y_left) * y_left_size + mse(y_right) * y_right_size) / (y_right_size + y_left_size)
+    """Compute the weighted mse criterion for a given set of target values."""
+    num = mse(y_left) * y_left.size + mse(y_right) * y_right.size
+    den = y_left.size + y_right.size
+    return num / den
+
+
+def split(X: np.ndarray, y: np.ndarray, feature: int) -> float:
+    """Find the best split for a node."""
+    node_size = y.size
+    if node_size < 2:
+        return None, None
+
+    node_mse = mse(y)
+    best_mse = node_mse
+    best_thr = None
+
+    thresholds = np.unique(X[:, feature])
+    for thr in thresholds:
+        left = y[X[:, feature] <= thr]
+        right = y[X[:, feature] > thr]
+
+        if left.size == 0 or right.size == 0:
+            continue
+
+        weihted_mse = weighted_mse(left, right)
+        if weihted_mse < best_mse:
+            best_mse = weihted_mse
+            best_thr = thr
+
+    return best_thr
