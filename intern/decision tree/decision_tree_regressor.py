17bbe2cbe2e4843233e1899f717e02e53ef05820
Neesty <mysteriousneutrino@gmail.com>
2024-01-17 13:31:50 +0500

    decision tree 6 ver2 | done

--- intern/decision tree/decision_tree_regressor.py
+++ intern/decision tree/decision_tree_regressor.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 
 import numpy as np
-
+import pandas as pd
 
 @dataclass
 class Node:
@@ -13,7 +13,7 @@ class Node:
     threshold: float = None
     n_samples: int = None
     value: int = None
-    mse: int = None
+    mse: float = None
     left: Node = None
     right: Node = None
 
@@ -32,52 +32,91 @@ class DecisionTreeRegressor:
 
     def _mse(self, y: np.ndarray) -> float:
         """Compute the mse criterion for a given set of target values."""
-        return np.mean((y - np.mean(y)) ** 2)
+        if len(y) == 0:
+            return 0.0
+        return float(np.mean((y - np.mean(y)) ** 2))
 
     def _weighted_mse(self, y_left: np.ndarray, y_right: np.ndarray) -> float:
         """Compute the weighted mse criterion for a two given sets of target values"""
+        # if len(y_left) == 0 or len(y_right) == 0:
+        #     return 0.0
         num = self._mse(y_left) * y_left.size + self._mse(y_right) * y_right.size
         den = y_left.size + y_right.size
         return num / den
 
     def _best_split(self, X: np.ndarray, y: np.ndarray) -> tuple[int, float]:
         """Find the best split for a node."""
-        best_split = {}
-        for feature in range(X.shape[1]):
-            split_elements = X[:, feature]
+        node_size = y.size
+        n_features_ = X.shape[1]
+        if node_size < 2:
+            return None, None
 
-            best_split_1_future = {}
-            for i in split_elements:
-                indexes_below_split_value = np.where(split_elements <= i)[0]
-                indexes_above_split_value = np.where(split_elements > i)[0]
+        node_mse = self._mse(y)
+        best_mse = node_mse
+        best_idx, best_thr = None, None
 
-                y_left = np.array(y[indexes_below_split_value])
-                y_right = np.array(y[indexes_above_split_value])
+        for idx in range(n_features_):
+            thresholds = np.unique(X[:, idx])
+            for thr in thresholds:
+                left = y[X[:, idx] <= thr]
+                right = y[X[:, idx] > thr]
 
-                mse = self._weighted_mse(y_left, y_right)
-                best_split_1_future[i] = mse
+                if left.size == 0 or right.size == 0:
+                    continue
 
-            best_split[feature] = min(best_split_1_future, key=best_split_1_future.get)
+                weihted_mse = self._weighted_mse(left, right)
+                if weihted_mse < best_mse:
+                    best_mse = weihted_mse
+                    best_idx = idx
+                    best_thr = thr
 
-        best_feature, best_thr = min(best_split.items(), key=lambda x: x[1])
-
-        return best_feature, best_thr
+        return best_idx, best_thr
 
     def _split_node(self, X: np.ndarray, y: np.ndarray, depth: int = 0) -> Node:
         """Split a node and return the resulting left and right child nodes."""
+        # print("_split_node y: ", y)
         node = Node()
-        node.n_samples = y.size
-        node.value = np.mean(y)
-        node.mse = self._mse(y)
-        if depth == self.max_depth or y.size >= self.min_samples_split:
+        print(X)
+        print(y)
+        if depth == self.max_depth or len(np.unique(y)) == 1:
+            node.n_samples = y.size
+            node.value = int(round(np.mean(y)))
+            node.mse = self._mse(y)
             return node
-        node.feature, node.threshold = self._best_split(X, y)
-        depth += 1
-        if node.threshold is not None:
-            node.left = X[X[:, node.feature] <= node.threshold], y[X[:, node.feature] <= node.threshold]
-            self._split_node(node.left[0], node.left[1], depth)
-            node.right = X[X[:, node.feature] > node.threshold], y[X[:, node.feature] > node.threshold]
-            self._split_node(node.right[0], node.right[1], depth)
+
+        best_idx, best_thr = self._best_split(X, y)
+        node.feature = best_idx
+        node.threshold = best_thr
+        if best_idx is not None and best_thr is not None:
+            left_mask = X[:, best_idx] <= best_thr
+            right_mask = X[:, best_idx] > best_thr
+
+
+            node.n_samples = len(y)
+            node.value = int(round(np.mean(y)))
+
+            # Добавлены проверки на пустые подвыборки
+            node.mse = self._mse(y)
+            print(node)
+            node.left = self._split_node(X[left_mask], y[left_mask], depth + 1)
+            node.right = self._split_node(X[right_mask], y[right_mask], depth + 1)
         else:
             return node
         return node
+
+
+
+# X = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18], [19, 20, 21], [22, 23, 24],
+#               [25, 26, 27], [28, 29, 30]])
+# y = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+# df = pd.read_csv(r"C:\Users\Neesty\PycharmProjects\ml_sim_kc\intern\decision tree\load-delay_days_decision_tree_1000.csv")
+# df = df[:50]
+# X = df.drop(columns=['delay_days']).values
+# y = df['delay_days'].values
+#
+# decisionTreeRegressor = DecisionTreeRegressor(max_depth=2)
+#
+# model = decisionTreeRegressor.fit(X, y)
+#
+# print(model.tree_)
