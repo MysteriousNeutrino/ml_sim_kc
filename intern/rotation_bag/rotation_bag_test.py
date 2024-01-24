9e83578ef890888b76997e9359aa826ece13e36c
Neesty <mysteriousneutrino@gmail.com>
2024-01-24 13:10:30 +0500

    rotation_bag 2 ver1 | done

--- intern/rotation_bag/rotation_bag_test.py
+++ intern/rotation_bag/rotation_bag_test.py
@@ -1,4 +1,4 @@
-import utils
+import rotation_bag_1
 from skimage.data import stereo_motorcycle
 
 original_image, _, _ = stereo_motorcycle()
@@ -6,5 +6,5 @@ original_image, _, _ = stereo_motorcycle()
 
 def test_rotated_image():
 
-    result = utils.rotated_image(original_image, 180)
-    assert original_image.shape == result.shape
+    result = rotation_bag_1.rotated_image(original_image, 180)
+    assert result.shape == original_image.shape, f"{result.shape} != {result.shape}"
