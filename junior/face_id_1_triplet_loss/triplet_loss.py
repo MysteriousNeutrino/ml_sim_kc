45f3ff2cc82f6be7f9b176e3dee367434bdc4174
Neesty <mysteriousneutrino@gmail.com>
2024-02-26 12:16:52 +0500

    triplet_loss 3 ver1 |done

---
+++ junior/face_id_1_triplet_loss/triplet_loss.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+def triplet_loss(
+    anchor: np.ndarray, positive: np.ndarray, negative: np.ndarray, margin: float = 5.0
+) -> float:
+    """
+    Computes the triplet loss using numpy.
+    Using Euclidean distance as metric function.
+
+    Args:
+        anchor (np.ndarray): Embedding vectors of
+            the anchor objects in the triplet (shape: (N, M))
+        positive (np.ndarray): Embedding vectors of
+            the positive objects in the triplet (shape: (N, M))
+        negative (np.ndarray): Embedding vectors of
+            the negative objects in the triplet (shape: (N, M))
+        margin (float): Margin to enforce dissimilar samples to be farther apart than
+
+    Returns:
+        float: The triplet loss
+    """
+    point_distance_positive = np.linalg.norm(anchor - positive, axis=1)
+    point_distance_negative = np.linalg.norm(anchor - negative, axis=1)
+    L = np.maximum(0, point_distance_positive - point_distance_negative + margin)
+
+    return np.mean(L)
